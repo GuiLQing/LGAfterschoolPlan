9e2870bde8199443d92e8541cc17705520f470a6
PSG <gui950823@126.com>
2019-07-24 18:51:12 +0800

    1.1.3

--- LGAfterschoolPlanFramework/Classes/LGAfterschoolPlanFramework.framework/Headers/LGAfterschoolPlanFramework.h
+++ LGAfterschoolPlanFramework/Classes/LGAfterschoolPlanFramework.framework/Headers/LGAfterschoolPlanFramework.h
@@ -16,7 +16,5 @@ FOUNDATION_EXPORT const unsigned char LGAfterschoolPlanFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LGAfterschoolPlanFramework/PublicHeader.h>
 
-#import "APBaseNavigationController.h"
-#import "APStudyPlanListViewController.h"
 #import "LGAfterschoolPlanManager.h"
 
